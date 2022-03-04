cd4bd78a80f283d09d14abe1bb6496be9cd7c974
SR TAMIM <saifur.tamim@gmail.com>
2022-03-04 18:13:46 +0600

    shop component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createContext, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Shop from './components/Shop/Shop';
 
 export const CartContext = createContext()
 
@@ -9,12 +10,12 @@ function App() {
   const [cart, useCart] = useState([])
   return (
     <BrowserRouter><CartContext.Provider value={{ cart, useCart }}>
-      <div className="App">
-        <Navbar />
+      <Navbar />
+      <main className="App">
         <Routes>
-          <Route path="/" element={<h1>Happy clothings</h1>} />
+          <Route path="/" element={<Shop />} />
         </Routes>
-      </div>
+      </main>
     </CartContext.Provider></BrowserRouter>
   );
 }
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,31 @@
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <h1>Category name</h1>
+            <div className="shop_products">
+                <div className="shop_product">
+                    <h4>item1</h4>
+                </div>
+                <div className="shop_product">
+                    <h4>item2</h4>
+                </div>
+                <div className="shop_product">
+                    <h4>item3</h4>
+                </div>
+                <div className="shop_product">
+                    <h4>item4</h4>
+                </div>
+                <div className="shop_product">
+                    <h4>item5</h4>
+                </div>
+                <div className="shop_product">
+                    <h4>item6</h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
